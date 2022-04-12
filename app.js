618364723c4f7c6e6c16a0819297353e413bc907
Gianath <Gianathevan01@gmail.com>
2022-04-12 22:15:23 +0700

    Feat: Connect DB and Add DotENV

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const connectDB = require("./db/connect");
 
 const app = express();
 
@@ -7,12 +8,15 @@ const app = express();
 const register = require("./routes/register");
 const login = require("./routes/login");
 
+//ENV
+require("dotenv").config();
+
 // Middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
+// Routes
 app.use("/register", register);
-
 app.use("/login", login);
 
 // Dasboard (Nanti)
@@ -24,6 +28,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
