694d5f15bc5b50c5ce2794616ef263f697187014
Adrian-py <62276355+Adrian-py@users.noreply.github.com>
2022-04-12 16:59:31 +0700

    Created user schema and login/register controllers

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const {
+  getLoginPage,
+  checkCredentials,
+} = require("../controllers/loginController");
+
+router.get("/", getLoginPage);
+router.post("/api", checkCredentials);
 
 module.exports = router;
