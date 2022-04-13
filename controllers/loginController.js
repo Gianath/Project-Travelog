8b0ed0741a988417370d1a69c2550d67ff5db1e3
Gianath <Gianathevan01@gmail.com>
2022-04-13 22:38:04 +0700

    Feat: Validate Credentials

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,12 +1,35 @@
 const path = require("path");
+const bcrypt = require("bcrypt");
+
+// User Model
+const userModel = require("../models/user");
 
 const getLoginPage = (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "html", "login.html"));
 };
 
-const checkCredentials = (req, res) => {
-  console.log(req.body);
-  res.json({ status: ok, body: req.body });
+const checkCredentials = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const resp = await userModel.findOne({
+      email: email,
+    });
+    bcrypt.compare(password, resp.password, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else if (result) {
+        console.log("Succesfully login!\n", resp);
+        res.json({ status: "ok", body: req.body });
+      } else if (!result) {
+        console.log("Failed login!\n", resp);
+        res.json({ status: "Failed", body: req.body });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: "failed" });
+  }
 };
 
 module.exports = {
--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,22 @@
 const loginForm = document.getElementById("login-form");
 
-loginForm.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const email = document.getElementById("Email").value,
     password = document.getElementById("Password").value;
+
+  const res = await fetch("/login/api", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 
   console.log(email, password);
 });
