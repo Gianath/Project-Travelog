6adb38bc50ea973bd765933c108b3060d780c95e
Gianath <Gianathevan01@gmail.com>
2022-04-15 21:42:09 +0700

    Feat: Create Middleware for authentication

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = async (req, res, next) => {
+  const token = req.cookies.authToken;
+  if (!token) {
+    return res.json({ status: "failed", msg: "no token found" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.userID = id;
+    req.username = username;
+    next();
+  } catch (error) {
+    return res.json({ status: "failed", msg: error });
+  }
+};
