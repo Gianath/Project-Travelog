bf24c12972cb359c3be7d626481ad5f22d15615b
Gianath <Gianathevan01@gmail.com>
2022-05-24 09:26:05 +0700

    Fix: Remove likes and views from comment

--- models/comment.js
+++ models/comment.js
@@ -5,8 +5,6 @@ const commentSchema = new mongoose.Schema(
   {
     authorID: { type: Schema.Types.ObjectId, ref: "User" },
     content: { type: String, required: true },
-    likes: { type: Number, default: 0 },
-    views: { type: Number, default: 0 },
   },
   { collection: "Comments" }
 );
